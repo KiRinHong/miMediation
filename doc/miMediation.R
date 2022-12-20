422f203443e862e278469fefead842cefc78f40a
KirinHong <kirin.bbhong@gmail.com>
2022-12-20 16:56:55 -0500

    Add files via upload

--- doc/miMediation.R
+++ doc/miMediation.R
@@ -1,4 +1,8 @@
 ## ---- include = FALSE---------------------------------------------------------
+# pdf_document
+# prettydoc::html_pretty:
+#  theme: cayman
+#  highlight: github
 knitr::opts_chunk$set(
   warning=FALSE, message=FALSE,
   collapse = TRUE,
@@ -22,11 +26,10 @@ head(M[,1:6])
 Y <- data.cecal$outcome
 summary(Y)
 tree <- data.cecal$tree
-tree
 
-## ----tree-plot,fig.width=8, fig.height=8, fig.show='hold'---------------------
+## ----phy-tree-plot,fig.width=8, fig.height=8, fig.show='hold'-----------------
 # set random seed here so that you can get the same result every time you run the code
-set.seed(123)
+set.seed(84)
 cecal.rsltlst <- phyloMed(Trt, M, Y, tree, fdr.alpha = 0.1, n.perm = 1e5, graph = TRUE)
 
 # take a look at phyloseq-class object
@@ -35,4 +38,25 @@ cecal.physeq
 cecal.rslt <- cecal.rsltlst$rslt
 # take a look at rslt (PhyloMed.P)
 cecal.rslt$PhyloMed.P
+
+## -----------------------------------------------------------------------------
+# Load data
+data(data.zeeviD)
+# Take a look at the data
+Trt <- data.zeeviD$treatment
+table(Trt) # 0: control 1: treatment
+M <- data.zeeviD$mediators
+dim(M)
+Y <- data.zeeviD$outcome
+summary(Y)
+tree <- data.zeeviD$tree
+head(tree)
+
+## ----tax-tree-plot,fig.width=8, fig.height=8, fig.show='hold',message=FALSE,warning=FALSE----
+# only show aysmptotic result
+demo.rsltlst <- phyloMed(Trt, M, Y, tree, graph = TRUE)
+# take a look at phyloseq-class object
+demo.physeq <- demo.rsltlst$clean.data
+demo.physeq
+demo.rsltlst$rslt$PhyloMed.A
 
